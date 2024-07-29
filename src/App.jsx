aa7b9b25dde46af99048ad438b899c85e4c56e0e
jonathan296 <55729464+jonathan296@users.noreply.github.com>
2024-07-29 10:07:15 -0700

    Hero page changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,68 +1,88 @@
-import { useState, useContext } from 'react'
-import Products from './components/Products'
-import { CartContext } from './context/cart'
-import './index.css'
-import { Routes, Route, Link } from 'react-router-dom'
-import Details from './components/Details'
-import Cart from './components/Cart'
-import icon from './assets/react.svg'
-import Home from './components/Home'
-import Login from './components/Login'
-import Register from './components/Register'
-
-
+import { useState, useContext } from "react";
+import Products from "./components/Products";
+import { CartContext } from "./context/cart";
+import "./index.css";
+import { Routes, Route, Link } from "react-router-dom";
+import Details from "./components/Details";
+import Cart from "./components/Cart";
+import icon from "./assets/react.svg";
+import Home from "./components/Home";
+import Login from "./components/Login";
+import Register from "./components/Register";
 
 function App() {
-  const { cartItems } = useContext(CartContext)
-  const [showModal, setShowModal] = useState(false)
+  const { cartItems } = useContext(CartContext);
+  const [showModal, setShowModal] = useState(false);
 
-  
-    const toggle = () => {
-        setShowModal(!showModal)
-        
-      }
+  const toggle = () => {
+    setShowModal(!showModal);
+  };
 
   return (
-    <div className='bg-gray-100 '>
-        
-        <div className='relative bg-[#191919]  h-[729px] px-[165px] pt-[32px] flex flex-col  text-[#FFFFFF]'>
-          <div className=' z-10 flex justify-between items-center mb-[36px]'>
-            <div>
-            <img src='src\assets\shared\desktop\logo.svg'></img>
-            </div>
-            <div className=' flex gap-[34px] uppercase manrope leading-[25px] font-bold tracking-[2px] text-[13px] '>
-            <Link to='/'className=''>Home</Link>
-            <Link to='/login'className=''>HEADPHONES</Link>
-            <Link to='/login'className=' '>SPEAKERS</Link>
-            <Link to='/login'className=' '>EARPHONES</Link>
-            
-            </div>
+    <div>
+      <div className="bg-[#191919]">
+        <header className="relative flex flex-col items-center  ">
+          <div className="h-[729px]  pt-[32px] lg:px-0 px-6 flex flex-col max-w-[1110px] w-full m-auto text-[#FFFFFF]">
+            <div className="z-10 flex  justify-between items-center mb-[36px]">
+              <div>
+                <img src="src\assets\shared\desktop\logo.svg"></img>
+              </div>
+              <div className=" flex gap-[34px] uppercase manrope leading-[25px] font-bold tracking-[2px] text-[13px] ">
+                <Link to="/" className="">
+                  Home
+                </Link>
+                <Link to="/login" className="">
+                  HEADPHONES
+                </Link>
+                <Link to="/login" className=" ">
+                  SPEAKERS
+                </Link>
+                <Link to="/login" className=" ">
+                  EARPHONES
+                </Link>
+              </div>
 
-            <div>
-              {!showModal && <button className='  text-white hover:bg-gray-700 focus:outline-none focus:bg-gray-700'
-            onClick={toggle}> <img src='src\assets\shared\desktop\icon-cart.svg'></img>
-            {/* Cart ({cartItems.length}) */}
-            </button>}
+              <div className="relative">
+                {!showModal && (
+                  <button
+                    className="  text-white hover:bg-gray-700 focus:outline-none focus:bg-gray-700"
+                    onClick={toggle}
+                  >
+                    {" "}
+                    <img src="src\assets\shared\desktop\icon-cart.svg"></img>
+                    <div className="bg-orange-400 rounded-full absolute inset-0">
+                      {cartItems.length}
+                    </div>
+                  </button>
+                )}
+              </div>
             </div>
-           
+            <hr className="z-10  border-[#FFFFFF] opacity-20 " />
           </div>
-          <hr className="w-[1110px] z-10 border-[#FFFFFF] opacity-20 " />
-          <img className='absolute m-auto inset-0 ' src='src\assets\home\desktop\image-hero.jpg'></img>
-          
+          <img
+            className=" absolute m-auto object-cover h-full inset-0 "
+            src="src\assets\home\desktop\image-hero.jpg"
+          ></img>
+        </header>
+        <Cart showModal={showModal} toggle={toggle} />
+      </div>
+      <section className="m-3">
+        <div className="flex gap-3 justify-center">
+          <div className="bg-slate-800 w-[350px] h-[204px]"> </div>
+          <div className="bg-slate-800 w-[350px] h-[204px]"> </div>
+          <div className="bg-slate-800 w-[350px] h-[204px]"> </div>
         </div>
-       
-        <div>
-          <Routes>
-            <Route path='/details/:id' element={<Details/>}/>
-            <Route path='/' element={<Products/>}/>
-            <Route path='/login' element={<Login/>}/>
-            <Route path='/register' element={<Register/>}/>
-          </Routes>
-        </div>
-
-        <Cart showModal={showModal} toggle={toggle}/>
+      </section>
+      {/* <div>
+        <Routes>
+          <Route path="/details/:id" element={<Details />} />
+          <Route path="/" element={<Products />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </div> */}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
